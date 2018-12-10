2bf7f8282cd7e278591cb6a9151e57820fda34a7
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2018-12-10 23:07:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="calculator">
+          <div id="display">
+            <span id="statement">6-3=3</span>
+            <span id="inputElement">3</span>
+          </div>
+            
+          <div id="pad">
+            <button id="clear" className="dark">AC</button>
+            <button id="plus-negative" className="dark">+/-</button>
+            <button id="percentage" className="dark">%</button>
+            <button id="divide">/</button>
+            <button id="seven" className="number">7</button>
+            <button id="eight" className="number">8</button>
+            <button id="nine" className="number">9</button>
+            <button id="multiply">X</button>
+            <button id="four" className="number">4</button> 
+            <button id="five" className="number">5</button>
+            <button id="six" className="number">6</button>
+            <button id="subtract">-</button>
+            <button id="one" className="number">1</button>
+            <button id="two" className="number">2</button>
+            <button id="three" className="number">3</button>
+            <button id="add">+</button>
+            <button id="zero" className="number">0</button>
+            <button id="decimal" className="number">.</button>
+            <button id="equals">=</button>
+          </div>
+
+        </div>
+        <span id="signature">Designed and Coded by Alex Disdier</span>
       </div>
     );
   }
