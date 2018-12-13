b30c84419746a67f7d35972847c27624bd3fd12d
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2018-12-13 01:12:23 +0100

    my code was getting so confusing, I decided to start with a clean slate. Not only my code is a lot more organized, it makes perfect sense and it's easier to maintain and move forward with the functionalities. Can't wait to carry on.

--- src/App.js
+++ src/App.js
@@ -3,119 +3,135 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
-  state = {
-    inputValue: '0',
-    outputDisplay: '',
-    equal: false
+  constructor(props){
+    super(props);
+    this.state = {
+      currentValue: '0',
+      previousValue: '0',
+      currentSign: '',
+      lastClicked: '',
+      computingDisplay: '',
+      currentDisplay: '0'
+    }
+
+    this.operatorsHandler = this.operatorsHandler.bind(this);
+    this.numbersHandler = this.numbersHandler.bind(this);
+    this.decimalHandler = this.decimalHandler.bind(this);
+    this.percentageHandler = this.percentageHandler.bind(this);
+    this.negativePositiveHandler = this.negativePositiveHandler.bind(this);
+    this.computeHandler = this.computeHandler.bind(this);
+    this.resetHandler = this.resetHandler.bind(this);
   }
 
-  equalClickedHandler = () => {
-    this.inputValueHandler('='); // updates the display with = sign
-
-    const opsRegex = /\+|-|\*|\//gm; // Operators regex
-
-    const ops = { // functions applying the operators
-      '+': (a, b) => a + b, 
-      '-': (a, b) => a - b, 
-      '*': (a, b) => a * b, 
-      '/': (a, b) => a / b, 
-    };
-
-    const checkEqual = this.state.equal; 
-
-    const outputDisplay = this.state.outputDisplay.slice(); 
-
-    const operatorExistArray = outputDisplay.match(opsRegex);
-
-    let positionOperator = outputDisplay.search(opsRegex); 
-
-    let currentOperator = outputDisplay[positionOperator]; 
-
-    let firstNum = parseFloat(outputDisplay.substring(0,positionOperator));
-
-    let secondNum = parseFloat(outputDisplay.substring(positionOperator+1));
-
-    let result;
-    if (currentOperator === '+'){
-      result = (ops['+'](firstNum,secondNum));
-    }
-    else if (currentOperator === '-'){
-      result = (ops['-'](firstNum,secondNum));
-    }
-    else if (currentOperator === '*'){
-      result = (ops['*'](firstNum,secondNum));
-    }
-    else if (currentOperator === '/'){
-      result = (ops['/'](firstNum,secondNum));
-    }
-
-    this.setState({ 
-      equal: !checkEqual,
-      inputValue: result, 
-      outputDisplay: outputDisplay + '=' + result 
+  operatorsHandler = (event) => {
+    this.setState({
+      currentSign: event.target.value,
+      lastClicked: event.target.value,
+      currentDisplay: event.target.value
     })
   }
 
-  inputValueHandler = (e) => {
-    const outputDisplay = this.state.outputDisplay.slice();
+  numbersHandler = (event) => {
+    // console.log(event.target.value);
+    this.setState({
+      previousValue: this.state.currentValue,
+      currentValue: event.target.value,
+      lastClicked: event.target.value,
+      currentDisplay: event.target.value
+    })
+  }
 
-    if (!this.state.equal){
-      if (outputDisplay[0] === '0'){
-        outputDisplay.slice(1);
-      }
-      this.setState({ 
-        outputDisplay: outputDisplay + e,
-        inputValue: e
-      })
-    } else {
-      this.setState({ 
-        outputDisplay: e,
-        inputValue: e,
-        equal: false
-      })
+  decimalHandler = (event) => {
+    // console.log(event.target.value);
+    this.setState({
+      currentValue: event.target.value,
+      currentDisplay: event.target.value
+    })
+  }
+
+  percentageHandler = (event) => {
+    // console.log(event.target.value);
+    this.setState({
+      currentValue: event.target.value,
+      currentDisplay: event.target.value
+    })
+  }
+
+  negativePositiveHandler = (event) => {
+    // console.log(event.target.value);
+    const negativePositive = -this.state.currentValue;
+    this.setState({
+      currentValue: negativePositive,
+      currentDisplay: negativePositive
+    })
+  }
+
+  computeHandler = () => {
+    let result;
+
+    if (this.state.currentSign === '+'){
+      result = parseFloat(this.state.previousValue) + parseFloat(this.state.currentValue);
+
+    } else if (this.state.currentSign === '-'){
+      result = parseFloat(this.state.previousValue) - parseFloat(this.state.currentValue);
+
+    } else if (this.state.currentSign === '*'){
+      result = parseFloat(this.state.previousValue) * parseFloat(this.state.currentValue);
+
+    } else if (this.state.currentSign === '/'){
+      result = parseFloat(this.state.previousValue) / parseFloat(this.state.currentValue);
     }
+
+    this.setState({
+      currentValue: result,
+      currentDisplay: result
+    })
   }
 
-  negativePositiveHandler = (num) => {
-    this.setState({ inputValue: -num })
-    console.log('you clicked +/-: ' + num)
+  resetHandler = () => {
+    this.setState({
+      currentValue: '0',
+      previousValue: '0',
+      currentSign: '',
+      lastClicked: 'AC',
+      computingDisplay: '',
+      currentDisplay: '0'
+    })
   }
-
-  resetHandler = () => this.setState({ outputDisplay: [] , inputValue: 0});
 
   render() {
     return (
       <div className="App">
         <div id="calculator">
           <div id="display-flex">
-            <span id="output">{this.state.outputDisplay}</span>
-            <span id="display">{this.state.inputValue}</span>
+            <span id="output">{this.state.computingDisplay}</span>
+            <span id="display">{this.state.currentDisplay}</span>
           </div>
             
           <div id="pad">
             <button id="clear" className="dark" onClick={this.resetHandler}>AC</button>
-            <button id="plus-negative" className="dark" onClick={() => this.negativePositiveHandler(this.state.inputValue)}>+/-</button>
-            <button id="percentage" className="dark">%</button>
-            <button id="divide" onClick={ () => this.inputValueHandler('/')}>/</button>
-            <button id="seven" className="number" onClick={ () => this.inputValueHandler('7')}>7</button>
-            <button id="eight" className="number" onClick={ () => this.inputValueHandler('8')}>8</button>
-            <button id="nine" className="number" onClick={ () => this.inputValueHandler('9')}>9</button>
-            <button id="multiply" onClick={ () => this.inputValueHandler('*')}>X</button>
-            <button id="four" className="number" onClick={ () => this.inputValueHandler('4')}>4</button> 
-            <button id="five" className="number" onClick={ () => this.inputValueHandler('5')}>5</button>
-            <button id="six" className="number" onClick={ () => this.inputValueHandler('6')}>6</button>
-            <button id="subtract" onClick={ () => this.inputValueHandler('-')}>-</button>
-            <button id="one" className="number" onClick={ () => this.inputValueHandler('1')}>1</button>
-            <button id="two" className="number" onClick={ () => this.inputValueHandler('2')}>2</button>
-            <button id="three" className="number" onClick={ () => this.inputValueHandler('3')}>3</button>
-            <button id="add" onClick={ () => this.inputValueHandler('+')}>+</button>
-            <button id="zero" className="number" onClick={ () => this.inputValueHandler('0')}>0</button>
-            <button id="decimal" className="number" onClick={ () => this.inputValueHandler('.')}>.</button>
-            <button id="equals" onClick={this.equalClickedHandler}>=</button>
+            <button id="plus-negative" value="+/-" className="dark" onClick={this.negativePositiveHandler}>+/-</button>
+            <button id="percentage" value="%" className="dark" onClick={this.percentageHandler}>%</button>
+            <button id="divide" value="/" onClick={this.operatorsHandler}>/</button>
+            <button id="seven" value="7" className="number" onClick={this.numbersHandler}>7</button>
+            <button id="eight" value="8" className="number" onClick={this.numbersHandler}>8</button>
+            <button id="nine" value="9" className="number" onClick={this.numbersHandler}>9</button>
+            <button id="multiply" value="*" onClick={this.operatorsHandler}>x</button>
+            <button id="four" value="4" className="number" onClick={this.numbersHandler}>4</button> 
+            <button id="five" value="5" className="number" onClick={this.numbersHandler}>5</button>
+            <button id="six" value="6" className="number" onClick={this.numbersHandler}>6</button>
+            <button id="subtract" value="-" onClick={this.operatorsHandler}>-</button>
+            <button id="one" value="1" className="number" onClick={this.numbersHandler}>1</button>
+            <button id="two" value="2" className="number" onClick={this.numbersHandler}>2</button>
+            <button id="three" value="3" className="number" onClick={this.numbersHandler}>3</button>
+            <button id="add" value="+" onClick={this.operatorsHandler}>+</button>
+            <button id="zero" value="0" className="number" onClick={this.numbersHandler}>0</button>
+            <button id="decimal" value="." className="number" onClick={this.decimalHandler}>.</button>
+            <button id="equals" onClick={this.computeHandler}>=</button>
           </div>
 
         </div>
-        <span id="author">Designed and Coded by <a href="https://alexdisdier.fr" target="_blank" rel="noopener noreferrer">Alex Disdier</a></span>
+        {/* <span id="author">Designed and Coded by <a href="https://alexdisdier.fr" target="_blank" rel="noopener noreferrer">Alex Disdier</a></span> */}
       </div>
     );
   }
